89ec76e18b7a7865dd7cb4db6d730cf179166829
maedeh82 <maedeh82mahi@gmail.com>
2024-07-27 18:02:03 +0330

    add git

---
+++ script.js
@@ -0,0 +1,46 @@
+// console.log("maedeh")
+
+//برای اوردن کد های اچ تی ام ال کافیه علامت تعجب و اینتر را بزنیم تا بالا بیاورد
+//کد اچ تی ام ال که در قسمت بادی مینویسیم
+//<script src="script.js"></script>
+
+// متغیر ها
+// let name = "maedeh"
+// console . log (name)
+//برای اینکه کلمه اول را از کلمه دوم جدا کنیم به جای اسپیس کلمه دوم را با حرف بزرگ شروع کنیم اسم این کار 
+// camel noteyshen
+//است
+//اکر بخواهیم اسم کوچیک و فامیلی را ذکر کنیم
+// let fname = "maedeh"
+// let lname = "khanzadeh"
+// console . log (fname , lname)
+
+//ثابت
+// const name = "maedeh"
+// console . log (name)
+//وقتی از ثابت استفاده میکنیم نمیتوانیم مقدارش را تغییر دهیم ولی در متغر میتوانیم
+
+//تایپ ها
+// string
+// let name = "maede"
+
+// numbers
+// let age = 21
+
+// bool
+// let admin = true 
+// console . log (name , age , admin)
+
+//نوع دوم تاپ ها
+// apject
+let person = {
+    name : "maedeh" ,
+    age : 21 ,
+    admin : true 
+}
+// console . log (person)
+
+//اگر بخواهیم یکی از چیزایی ک نوشتیم را لاگ بگیرد
+// console . log (persin . name)
+//یا
+console . log (person ["name"])
