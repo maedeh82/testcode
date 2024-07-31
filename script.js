06a664d35649622613d05e2fee058e17df6f6510
maedeh82 <maedeh82mahi@gmail.com>
2024-07-31 13:53:43 +0330

    1 hours

--- script.js
+++ script.js
@@ -33,16 +33,87 @@
 
 //نوع دوم تاپ ها
 // apject
-let person = {
-    name : "maedeh" ,
-    age : 21 ,
-    admin : true 
-}
+// let person = {
+//     name : "maedeh" ,
+//     age : 21 ,
+//     admin : true 
+// }
 // console . log (person)
 
 //اگر بخواهیم یکی از چیزایی ک نوشتیم را لاگ بگیرد
 // console . log (persin . name)
 //یا
-console . log (person ["name"])
+//console . log (person ["name"])
 
-// maedeh
+// آرایه ها
+
+//let users = ["maedeh" , "ali"]
+//users[2] = "love"
+//console . log (users)
+// یا
+//console . log (users [0])
+//اگر بخوایم بدونیم کلا چند عنصر دارد
+//console . log (users.length)
+
+// function
+// function maedeh (){
+//     let black = 2 + 2
+//   console . log (black)
+// }
+// maedeh()
+
+// function maedeh(name , age ){
+//     console . log (" hello " + name + " you are " + age + " years old ")
+// }
+// maedeh(" maedeh " , 21)
+
+//عملگر های محاسباتی
+// + و - و / و * و %
+// let x = 2
+// let y = 5
+// console . log (x + y)
+// اگر دوتا + قبل از ایکس باشد یه دونه به ایکس اضافه میکند اگر بعد ایکس باشد خود ایکس را نشان میدهد
+// اگر دوتا ** هم بگذاریم توانش را میگیرد
+
+// عملگر های انتصاب
+// += و *= و /= و %= و -=
+// x *= y
+//یعنی مقدار جدید ایکس میشه ایکس ضربدر وای
+
+// عملگر های مقایشه ای
+// relational
+// <  >  <=  >=
+// let x = 1
+// console . log (x > 1)
+
+//equality
+// lose equality
+// ==  !=
+
+// strict equality
+// === !===
+// فرق دوتا مساوی با سه تا اینه که دوتا تایپ هاشون مهم نیست ولی سه تا هست 
+// console . log ("1" === "1")
+// console . loge ("1" == 1)
+
+// let maedeh = true
+// let bottontex = maedeh === true ? "dashbord" : login
+// console . log (bottontex)
+
+//عملگر های منطقی
+// logical and
+// &&
+// اکر حتی یکیش فالز باشد نتیجه فالز میشود
+// let mojodi = 5
+// let gheymat = 1000
+// let pol = 1500
+// let buy = mojodi > 0 && pol > gheymat
+// console . log (buy)
+
+// logical or
+// ||
+// حتی اگر یکیش درست باشد نتیجه هم درست میشود
+
+// not
+// ! 
+// درست را غلط میکند و برعکس
